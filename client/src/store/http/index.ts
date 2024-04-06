7dcba6242655be0e423c3b78edab536819c32b67
damirsch08 <damiralecia@gmail.com>
2024-04-06 16:44:22 +0300

    .

--- client/src/store/http/index.ts
+++ client/src/store/http/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = 'http://localhost:5000/api'
+export const API_URL = 'https://chatapp-pearl-eta.vercel.app/api'
 
 const $api = axios.create({
   withCredentials: true,
